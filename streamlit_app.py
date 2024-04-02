1fffbfc13cb4553217c5a90dfa6e9e85ab4f0922
philking2022 <101882673+philking2022@users.noreply.github.com>
2024-04-02 12:42:06 +0100

    Update streamlit_app.py
    Moved the nutrition advice into the ingredients list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# New section to display fruityvice nutrition information
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
 
 # Write directly to the app
@@ -35,6 +32,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
